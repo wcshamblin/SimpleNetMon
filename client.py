11e5cfc693b79eb47183c79ef254c77731e1cdaf
wcshamblin <will@shamblin.org>
2020-04-06 14:40:33 -0400

    Prepared SMTP send

--- client.py
+++ client.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-import csv
 import os
-from datetime import datetime
-from time import sleep
+import base64
 
 rserv='will@shamblin.org'
-
-dformat = '%R'
+hostname='home_workstation' #do not use commas, that is the delim
 phost='google.com'
-pingcsv = open('ping.csv', 'w', newline='')
-pingcsv.write('time,ping\n')
-writer = csv.writer(pingcsv, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
 pcache=[]
-cping=os.popen('ping -c 10 ' + phost).read().strip().split('\n')
+cping=os.popen('ping -c 10 -W 5 ' + phost).read().strip().split('\n')
 for row in cping[1:-4]:
     try:
         pcache.append((float(row.split(' ')[-2].split('=')[1])))
     except:
          pcache.append(0.0)
 pcache=[round(round(sum((pcache)), 2)/len(pcache), 2)]
-writer.writerow([str(datetime.now().strftime(dformat)), float(pcache[0])])
-pingcsv.flush()
-pingcsv.close()
-#scp to rserv
+
+print("SimpleNetMonHostname="+str(hostname)+",Ping="+str(pcache[0]))
